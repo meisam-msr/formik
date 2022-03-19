a17c022997cb3d257959d5f0d1954302d735041f
meisam <maghsoodiradmeisam12@gmail.com>
2022-03-19 23:41:31 +0330

    use getFieldProps

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -24,26 +24,14 @@ const SignUpform = () => {
       <form onSubmit={formik.handleSubmit}>
         <div className="formControl">
           <label>Name</label>
-          <input
-            type="text"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.name}
-            name="name"
-          />
+          <input type="text" {...formik.getFieldProps("name")} name="name" />
           {formik.errors.name && formik.touched.name && (
             <div className="error">{formik.errors.name}</div>
           )}
         </div>
         <div className="formControl">
           <label>Email</label>
-          <input
-            type="text"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.email}
-            name="email"
-          />
+          <input type="text" {...formik.getFieldProps("email")} name="email" />
           {formik.errors.email && formik.touched.email && (
             <div className="error">{formik.errors.email}</div>
           )}
@@ -52,9 +40,7 @@ const SignUpform = () => {
           <label>Password</label>
           <input
             type="text"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.password}
+            {...formik.getFieldProps("password")}
             name="password"
           />
           {formik.errors.password && formik.touched.password && (
