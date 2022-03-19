96f4b564dc59737f09d770d9c32216d031dd79c0
meisam <maghsoodiradmeisam12@gmail.com>
2022-03-19 18:01:10 +0330

    create signUpForm cmp

---
+++ src/components/SignUpForm.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+const SignUpform = () => {
+  const [userData, setUserData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const changeHandler = ({ target }) => {
+    setUserData({ ...userData, [target.name]: target.value });
+  };
+
+  return (
+    <div>
+      <form>
+        <div className="formControl">
+          <label>Name</label>
+          <input
+            type="text"
+            onChange={changeHandler}
+            value={userData.name}
+            name="name"
+          />
+        </div>
+        <div className="formControl">
+          <label>Email</label>
+          <input
+            type="text"
+            onChange={changeHandler}
+            value={userData.email}
+            name="email"
+          />
+        </div>
+        <div className="formControl">
+          <label>Password</label>
+          <input
+            type="text"
+            onChange={changeHandler}
+            value={userData.password}
+            name="password"
+          />
+        </div>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUpform;
